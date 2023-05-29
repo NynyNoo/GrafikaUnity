41982608140cd65564bd1d541965526dbff76b3c
Nyny <pepsi146@wp.pl>
2023-05-29 20:50:03 +0200

    zad5

---
+++ My project/Assets/zad6/Scripts/CharScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CharScript : MonoBehaviour {
+
+	private Animator animator;
+	private bool m_Jump;
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+	}
+	void Update () {
+		m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+		animator.SetBool("Nodding", m_Jump);
+	}
+}
